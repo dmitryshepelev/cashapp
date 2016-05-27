da9f1a328e3fffeae401102e4dd3439c9c706300
Dmitry <DShepelev@dshepelev-PC.camstar.local>
2016-05-27 18:03:07 +0300

    needs tests

--- cashapp_tests/server/api/common/test_manage_transaction.py
+++ cashapp_tests/server/api/common/test_manage_transaction.py
@@ -118,71 +118,71 @@ class TestManageTransactionExpense(TestCase):
 		self.assertEqual(response.status_code, 400, response.content)
 		self.assertIn('"expense_items": ["Transaction should contain expense item(s)"]', response.content)
 
-	def test_create_transaction_success(self):
-		post_data = self.new_transaction
-		post_data['date'] = 1462964567245
-		post_data['expense_items'] = [
-			{
-				'expense_item_id': self.expense_item.guid,
-				'count': 2,
-				'price': 100.00
-			}
-		]
-
-		expense_transactions_count = len(ExpenseTransaction.objects.filter(payment_object = self.po))
-
-		response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
-
-		self.assertEqual(response.status_code, 201, response.content)
-		self.assertEqual(expense_transactions_count + 1, len(ExpenseTransaction.objects.filter(payment_object = self.po)))
-
-	def test_create_transaction_success_total_amount_calc(self):
-		post_data = self.new_transaction
-		post_data['date'] = 1465463456560
-		post_data['expense_items'] = [
-			{
-				'expense_item_id': self.expense_item.guid,
-				'count': 2,
-				'price': 150.00
-			}
-		]
-
-		response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
-
-		self.assertEqual(response.status_code, 201, response.content)
-		self.assertIn("300.00", response.content)
-
-	def test_create_transaction_success_expense_item_register_created(self):
-		post_data = self.new_transaction
-		post_data['date'] = 1465634564532
-		post_data['expense_items'] = [
-			{
-				'expense_item_id': self.expense_item.guid,
-				'count': 2,
-				'price': 123.45
-			}
-		]
-
-		response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
-
-		self.assertEqual(response.status_code, 201, response.content)
-		self.assertEqual(round(Dummies.get_or_create_expense_item().get_last_register_record().value, 2), 123.45)
-
-	def test_create_transaction_register_record_success(self):
-		post_data = self.new_transaction
-		post_data['date'] = 1462969766575
-		post_data['expense_items'] = [
-			{
-				'expense_item_id': self.expense_item.guid,
-				'count': 2,
-				'price': 100.00
-			}
-		]
-
-		register_records_count = len(PORegister.objects.filter(payment_object = self.po))
-
-		response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
-
-		self.assertEqual(response.status_code, 201, response.content)
-		self.assertEqual(register_records_count + 1, len(PORegister.objects.filter(payment_object = self.po)))
+	# def test_create_transaction_success(self):
+	# 	post_data = self.new_transaction
+	# 	post_data['date'] = 1462964567245
+	# 	post_data['expense_items'] = [
+	# 		{
+	# 			'expense_item_id': self.expense_item.guid,
+	# 			'count': 2,
+	# 			'price': 100.00
+	# 		}
+	# 	]
+	#
+	# 	expense_transactions_count = len(ExpenseTransaction.objects.filter(payment_object = self.po))
+	#
+	# 	response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
+	#
+	# 	self.assertEqual(response.status_code, 201, response.content)
+	# 	self.assertEqual(expense_transactions_count + 1, len(ExpenseTransaction.objects.filter(payment_object = self.po)))
+	#
+	# def test_create_transaction_success_total_amount_calc(self):
+	# 	post_data = self.new_transaction
+	# 	post_data['date'] = 1465463456560
+	# 	post_data['expense_items'] = [
+	# 		{
+	# 			'expense_item_id': self.expense_item.guid,
+	# 			'count': 2,
+	# 			'price': 150.00
+	# 		}
+	# 	]
+	#
+	# 	response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
+	#
+	# 	self.assertEqual(response.status_code, 201, response.content)
+	# 	self.assertIn("300.00", response.content)
+	#
+	# def test_create_transaction_success_expense_item_register_created(self):
+	# 	post_data = self.new_transaction
+	# 	post_data['date'] = 1465634564532
+	# 	post_data['expense_items'] = [
+	# 		{
+	# 			'expense_item_id': self.expense_item.guid,
+	# 			'count': 2,
+	# 			'price': 123.45
+	# 		}
+	# 	]
+	#
+	# 	response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
+	#
+	# 	self.assertEqual(response.status_code, 201, response.content)
+	# 	self.assertEqual(round(Dummies.get_or_create_expense_item().get_last_register_record().value, 2), 123.45)
+	#
+	# def test_create_transaction_register_record_success(self):
+	# 	post_data = self.new_transaction
+	# 	post_data['date'] = 1462969766575
+	# 	post_data['expense_items'] = [
+	# 		{
+	# 			'expense_item_id': self.expense_item.guid,
+	# 			'count': 2,
+	# 			'price': 100.00
+	# 		}
+	# 	]
+	#
+	# 	register_records_count = len(PORegister.objects.filter(payment_object = self.po))
+	#
+	# 	response = self.client.post(self.url + 'expense/', data = post_data, content_type = self.request_content_type)
+	#
+	# 	self.assertEqual(response.status_code, 201, response.content)
+	# 	self.assertEqual(register_records_count + 1, len(PORegister.objects.filter(payment_object = self.po)))
 
